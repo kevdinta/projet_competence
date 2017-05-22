4e63cc72cd7ba8f477420768d2074c4d82f097df
kevdinta <kevin.dintanavong@hotmail.fr>
2017-05-22 14:58:52 +0200

    22/05/2017 : Design OK Accueil, Bilan élève, créer épreuve, ajout des compétences.

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -20,3 +20,24 @@ $("#addCpt").click(function() {
     // Permet de lier le remove à l'input, mettre le content dans la div divcpt
     $("#divCpt").append(elem).append(removeLink)
 })
+
+function SearchBilan() {
+    let filter, table, tr, td, i
+    // search fct en maj ou minuscule carac
+    filter = input.value.toUpperCase()
+    table = document.getElementById("data_bilan_eleve")
+    tr = table.getElementsByTagName("tr")
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0]
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = ""
+            } else {
+                tr[i].style.display = "none"
+            }
+        }
+    }
+}
+let input
+input = document.getElementById("search");
+input.addEventListener('keyup', SearchBilan)
