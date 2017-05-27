3d2b7479a9db3dcf66df3a760a2782e8df4aaf04
kevdinta <kevin.dintanavong@hotmail.fr>
2017-05-27 14:29:24 +0200

    un peu d'es6

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,6 +1,47 @@
 /**
  * Created by Kevin on 17/05/2017.
  */
+
+/* Méthode de Foundation permettant d'afficher la sidebar */
+$(document).foundation()
+/* Plugin pour la date, changement du format (car nativement calendrier EN */
+$('.fdatepicker').fdatepicker({
+    language: 'fr',
+    format: 'dd/mm/yyyy'
+});
+/* Exécution de la méthode select2 permettant une recherche dans un select */
+$('.select2').select2()
+/* Exécution de la méthode confirm du plugin confirm.js permettant d'ouvrir un modal et afficher deux bouttons permettant de trigger 2 evenements */
+$('.blabla').confirm({
+    title: 'What is up?',
+    content: 'Here goes a little content',
+    type: 'red',
+    buttons: {
+        ok: {
+            text: "ok!",
+            keys: ['enter'],
+            action: function(){
+                console.log('the user clicked confirm')
+            }
+        },
+        cancel: function(){
+            console.log('the user clicked cancel')
+        }
+    }
+});
+/* Permet d'exécuter sur toute la page la méthode scrollUp() permettant alors lorsque la page est longue, de pouvoir faire un back to top */
+$(function(){
+    $.scrollUp()
+});
+/* Utilisation du plugin permettant un rating des notes, en choisissant comme design 'bars-movie' (fichier css) rating pour les select */
+$(function() {
+    $('#example').barrating({
+        theme: 'bars-movie'
+    });
+    $('#example2').barrating({
+        theme: 'bars-movie'
+    });
+});
 let inc = 2
 // Au clic de "Add cpt"
 $("#addCpt").click(function() {
@@ -19,6 +60,7 @@ $("#addCpt").click(function() {
     // Permet de lier le remove à l'input, mettre le content dans la div divcpt
     $("#divCpt").append(elem).append(removeLink)
 })
+/* Fonction permettant de rechercher une compétence en particulier */
 function InitializeSearch() {
     let input
     input = document.querySelector("#search")
@@ -40,16 +82,19 @@ function InitializeSearch() {
         }
     })
 }
+/* Fonction qui affiche div ou la cache si la value est différente */
 function DisplayBloc() {
     let inputDisplay
     inputDisplay = document.querySelector('#epreuve')
     inputDisplay.addEventListener('change', () => {
-        var style = inputDisplay.value == "BTS_SN" ? 'block' : 'none';
-        document.getElementById('bloc_eval').style.display = style;
+        /* boucle ternaire */
+        let style = inputDisplay.value == "BTS_SN" ? 'block' : 'none'
+        document.querySelector('#bloc_eval').style.display = style
     })
 }
 $(function () // on document.ready() - Execute la boucle if quand la page entière est chargée
 {
+    /* Permet d'exécuter une fonction si le selecteur est trouvé */
     if ($('table#data_bilan_eleve').length > 0)
     {
         InitializeSearch()
@@ -59,18 +104,17 @@ $(function () // on document.ready() - Execute la boucle if quand la page entiè
         DisplayBloc()
     }
 })
-$(document).ready(function test() {
+$(document).ready(() => {
     /* A chaque fois que ça scroll */
     $(window).scroll( function(){
         /* On vérifie pour chaque élément de classe hideme sa position */
         $('.hideme').each( function(i){
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height() + 200;
-            /* If the object is completely visible in the window, fade it it */
+            let bottom_of_object = $(this).offset().top + $(this).outerHeight()
+            let bottom_of_window = $(window).scrollTop() + $(window).height() + 200
+            /* Si l'objet est complètement visible dans la fenêtre, on la rend visible */
             if( bottom_of_window > bottom_of_object && $('div').is('#bloc_accueil') ){
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},500)
             }
-        });
-    });
-});
+        })
+    })
+})
