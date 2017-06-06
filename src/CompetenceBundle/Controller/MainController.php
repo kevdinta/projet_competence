6ae3cc745e02f6732e0d3a51e2f4c04f0cb8997f
kevdinta <kevin.dintanavong@hotmail.fr>
2017-06-06 20:21:49 +0200

    twig

--- src/CompetenceBundle/Controller/MainController.php
+++ src/CompetenceBundle/Controller/MainController.php
@@ -2,36 +2,108 @@
 
 namespace CompetenceBundle\Controller;
 // Ce use permet d'hériter du contrôleur de base
+use CompetenceBundle\CompetenceBundle;
+use CompetenceBundle\Entity\Competence;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Console\Helper\ProgressBar;
+use \PDO;
+
 
 class MainController extends Controller
 {
+    public function dbConnect(){
+        return $dbh = new PDO('mysql:host=localhost;dbname=projet_competence', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    }
+
     public function loginAction()
     {
         $content = $this->get('templating')
             ->render('CompetenceBundle:Pages:login.html.twig');
         return new Response($content);
     }
+
+
+
+
+
+
     public function indexAction()
     {
         $content = $this->get('templating')
                         ->render('CompetenceBundle:Pages:index.html.twig');
         return new Response($content);
     }
+
+
+
+
+
+
     public function bilanAction()
     {
+
+        //Liste des matières
+        $matieres =  $this->getListeMatiere();
+        var_dump($matieres);
+        //J'utilise var_dump pour afficher les valeur de la variable sur le template
+
+
+
+        //Si j'arrive a passer la variable contenant tout les nom au template
+        //On boucle dans le twig (Comme dans l'exemple menu.html.twig)
+
         $content = $this->get('templating')
-            ->render('CompetenceBundle:Pages:bilan.html.twig');
+            ->render('CompetenceBundle:Pages:bilan.html.twig', array( 'matieres' => $matieres));
         return new Response($content);
     }
+
+
+
+    public function getListeMatiere()
+    {
+        $dbh = $this->dbConnect();
+        $liste ="";
+
+
+        $sth = $dbh->prepare("SELECT nom_matiere FROM matiere");
+        $sth->execute();
+
+        /* Récupération de toutes les lignes d'un jeu de résultats */
+        $result = $sth->fetchAll(PDO::FETCH_COLUMN, 0);
+        /*foreach( $result as $matiere) {
+
+            $liste .= "<option> $matiere </option>";
+
+        }
+
+        return $liste;*/
+
+        return $result;
+
+
+    }
+
+
+
+
+
+
     public function bilan_eleveAction()
     {
         $content = $this->get('templating')
             ->render('CompetenceBundle:Pages:bilan_eleve.html.twig');
+
+
         return new Response($content);
     }
+
+
+
+
+
+
+
     public function evaluerAction()
     {
         $content = $this->get('templating')
@@ -56,7 +128,13 @@ class MainController extends Controller
             ->render('CompetenceBundle:Pages:autoevaluer.html.twig');
         return new Response($content);
     }
-    /*
+    public function evaluer_simplementAction()
+    {
+        $content = $this->get('templating')
+            ->render('CompetenceBundle:Pages:evaluer_simplement.html.twig');
+        return new Response($content);
+    }
+
     public function menuAction()
     {
         $listMenu = array(
@@ -70,5 +148,5 @@ class MainController extends Controller
             'listMenu' => $listMenu
         ));
     }
-    */
+
 }
--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,6 +1,47 @@
 /**
  * Created by Kevin on 17/05/2017.
  */
+
+/* Méthode de Foundation permettant d'afficher la sidebar */
+$(document).foundation()
+/* Plugin pour la date, changement du format (car nativement calendrier EN */
+$('.fdatepicker').fdatepicker({
+    language: 'fr',
+    format: 'dd/mm/yyyy'
+});
+/* Exécution de la méthode select2 permettant une recherche dans un select */
+$('.select2').select2()
+/* Exécution de la méthode confirm du plugin confirm.js permettant d'ouvrir un modal et afficher deux bouttons permettant de trigger 2 evenements */
+$('.blabla').confirm({
+    title: 'What is up?',
+    content: 'Here goes a little content',
+    type: 'red',
+    buttons: {
+        ok: {
+            text: "ok!",
+            keys: ['enter'],
+            action: function(){
+                console.log('the user clicked confirm')
+            }
+        },
+        cancel: function(){
+            console.log('the user clicked cancel')
+        }
+    }
+});
+/* Permet d'exécuter sur toute la page la méthode scrollUp() permettant alors lorsque la page est longue, de pouvoir faire un back to top */
+$(function(){
+    $.scrollUp()
+});
+/* Utilisation du plugin permettant un rating des notes, en choisissant comme design 'bars-movie' (fichier css) rating pour les select */
+$(function() {
+    $('#example').barrating({
+        theme: 'bars-movie'
+    });
+    $('#example2').barrating({
+        theme: 'bars-movie'
+    });
+});
 let inc = 2
 // Au clic de "Add cpt"
 $("#addCpt").click(function() {
@@ -10,7 +51,7 @@ $("#addCpt").click(function() {
         id: inc++,
         placeholder:"Votre nouvelle compétence..."
     })
-    let removeLink = $("<button>").html("Retirer").addClass("button columns row").click(function(){
+    let removeLink = $("<button>").html("Retirer").addClass("button columns row ").click(function(){
         // Supprime l'input text
         $(elem).remove()
         // Supprime le bouton supprimer
@@ -19,6 +60,7 @@ $("#addCpt").click(function() {
     // Permet de lier le remove à l'input, mettre le content dans la div divcpt
     $("#divCpt").append(elem).append(removeLink)
 })
+/* Fonction permettant de rechercher une compétence en particulier */
 function InitializeSearch() {
     let input
     input = document.querySelector("#search")
@@ -40,16 +82,19 @@ function InitializeSearch() {
         }
     })
 }
+/* Fonction qui affiche div ou la cache si la value est différente */
 function DisplayBloc() {
     let inputDisplay
     inputDisplay = document.querySelector('#epreuve')
     inputDisplay.addEventListener('change', () => {
-        var style = inputDisplay.value == "BTS_SN" ? 'block' : 'none';
-        document.getElementById('bloc_eval').style.display = style;
+        /* boucle ternaire */
+        let style = inputDisplay.value == "BTS_SN" ? 'block' : 'none'
+        document.querySelector('#bloc_eval').style.display = style
     })
 }
 $(function () // on document.ready() - Execute la boucle if quand la page entière est chargée
 {
+    /* Permet d'exécuter une fonction si le selecteur est trouvé */
     if ($('table#data_bilan_eleve').length > 0)
     {
         InitializeSearch()
@@ -59,18 +104,27 @@ $(function () // on document.ready() - Execute la boucle if quand la page entiè
         DisplayBloc()
     }
 })
-$(document).ready(function() {
+$(document).ready(() => {
     /* A chaque fois que ça scroll */
     $(window).scroll( function(){
         /* On vérifie pour chaque élément de classe hideme sa position */
         $('.hideme').each( function(i){
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height() + 300;
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},500);
+            let bottom_of_object = $(this).offset().top + $(this).outerHeight()
+            let bottom_of_window = $(window).scrollTop() + $(window).height() + 200
+            /* Si l'objet est complètement visible dans la fenêtre, on la rend visible */
+            if( bottom_of_window > bottom_of_object && $('div').is('#bloc_accueil') ){
+                $(this).animate({'opacity':'1'},500)
             }
-        });
-    });
-});
+        })
+    })
+    $("#checkbox").click(function(){
+        if($("#checkbox").is(':checked') ){
+            $("#etudiant > option").prop("selected","selected")
+            $("#etudiant").trigger("change")
+        }else{
+            $("#etudiant > option").prop("selected", false)
+            // permet de déclencher l'event change du select appelé étudiant
+            $("#etudiant").trigger("change")
+        }
+    })
+})
